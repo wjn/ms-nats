750b587007c57a57298a3e0750fbe87b5f0adcbb
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-10-06 09:13:03 -0500

    ticket-created-listener comments and console.log

--- src/events/ticket-created-listener.ts
+++ src/events/ticket-created-listener.ts
@@ -4,17 +4,22 @@ import { TicketCreatedEvent } from '../interfaces/ticket-created-event';
 import { Topics } from '../enums/topics';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  // by providing both the type (:type) and the assignment (= assignment) we
+  // by providing both the `readonly` and the assignment (= assignment) we
   // guarantee that the type and value of the topic for this class is always
-  // Topics.TicketCreated
-  topic: Topics.TicketCreated = Topics.TicketCreated;
+  // Topics.TicketCreated and cannot be changed in this class.
+  //
+  // `readonly` is similar to the `final` keyword in JAVA
+  readonly topic = Topics.TicketCreated;
 
   // NATS query group
   queueGroupName = 'payments-service';
 
   // process message received from NATS
-  onMessage(data: any, msg: Message) {
-    console.log('Event data!', data);
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log(`Event ${msg.getSequence()} data >>>`);
+    console.log(`ID: ${data.id}`);
+    console.log(`Title: ${data.title}`);
+    console.log(`Price: ${data.price}`);
 
     // indicate successful message parsed.
     msg.ack();
